15d5159fb192779ceb06debb96f1011a05c5197f
Pragya Mehrotra <pragyamehrotra0@gmail.com>
2024-03-11 23:46:44 +0530

    selectionSort Simple

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -18,11 +18,29 @@ public class SelectionSort {
         }
         return arr;
     }
+
+    public static int[] selectionSort(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
     
     public static void main(String[] args) {
         int arr[] = new int[]{8, 6, 2, 5, 1};
         int res[] = sort(arr);
         System.out.println(java.util.Arrays.toString(res));
-
+        int arr1[] = new int[]{10,9,9,8,4, 6, 2, 5, 1};
+        int res1[] = selectionSort(arr1);
+        System.out.println(java.util.Arrays.toString(res1));
     }
 }

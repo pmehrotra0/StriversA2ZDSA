8eacaabad69879409c2ee8dd9ebabffc5dea5772
Pragya Mehrotra <pragyamehrotra0@gmail.com>
2024-04-16 01:04:50 +0530

    rotated by k in BS

--- BinarySearch/MinRotated.java
+++ BinarySearch/MinRotated.java
@@ -1,5 +1,27 @@
 /* https://www.naukri.com/code360/problems/rotated-array_1093219 */
+/* https://www.naukri.com/code360/problems/rotation_7449070 */
 public class MinRotated {
+    public static int findKRotation(int []arr){
+        // Write your code here.
+        int min = Integer.MAX_VALUE;
+        int minind = -1;
+        int low = 0, high= arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(min > arr[mid]){
+                min = arr[mid];
+                minind = mid;
+            }
+            if(arr[mid] < arr[high]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+
+        }
+        return minind;
+    }
     public static int findMin(int []arr) {
         // Write your code here.
         int min = Integer.MAX_VALUE;
@@ -21,5 +43,6 @@ public class MinRotated {
     public static void main(String[] args) {
         int res=findMin(new int[]{7,8,9,1,2,3,4,5,6});
         System.out.println(res);
+        System.out.println(findKRotation(new int[]{7,8,9,1,2,3,4,5,6}));
     }
 }
